73fdf64c5036f0a31887267138a2f78243ef1670
Simone Ingenito <sim.ingenito@gmail.com>
2026-07-23 12:49:11 +0200

    feat(fe): Setup Infrastruttura Playwright e Cucumber

---
+++ cucumber.js
@@ -0,0 +1,9 @@
+module.exports = {
+  default: {
+    paths: ['e2e/features/**/*.feature'],
+    require: ['e2e/steps/**/*.ts'],
+    requireModule: ['ts-node/register'],
+    format: ['progress-bar', 'html:e2e/cucumber-report.html'],
+    publishQuiet: true
+  }
+}
